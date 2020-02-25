6a50ec5f51eaa656b1227721d11421e00bcc9168
Jordi Chulia <jorchube.dev@gmail.com>
2020-02-25 18:53:02 +0100

    Add some time measuring functions to engine

--- src/game_engine/engine/engine.py
+++ src/game_engine/engine/engine.py
@@ -1,3 +1,5 @@
+import time
+
 from src.game_engine import collision_engine
 from src.game_engine.game import Game
 
@@ -12,6 +14,7 @@ class Engine(object):
         self.engine_delegate.initialize(self)
         self._digested_events = []
         Game.set_display_configuration(display_configuration)
+        self.elapsed_ticks = 0
 
     @property
     def display_configuration(self):
@@ -27,6 +30,8 @@ class Engine(object):
         self.running = True
         while self.running:
             self._run_loop()
+            self.elapsed_ticks += 1
+            self.__print_timing_information()
 
     def _run_loop(self):
         actors = self._scene.actors()
@@ -54,3 +59,30 @@ class Engine(object):
         for actor in self._scene.actors():
             actor.receive_event(event)
 
+    def __print_timing_information(self):
+        if '_print_fps_start_ns' not in self.__dict__:
+            setattr(self, '_print_fps_start_ns', 0)
+        if self.elapsed_ticks % self.display_configuration.fps == 0:
+            print(f'>> {self.display_configuration.fps/((time.perf_counter_ns() - self._print_fps_start_ns)/1000000000.0)} FPS')
+            print(f'>> tick time: {(1000.0 / self.display_configuration.fps)} ms')
+            self._print_fps_start_ns = time.perf_counter_ns()
+
+    def _run_loop_measured(self):
+        actors = measure_ns(self._scene.actors)
+        measure_ns(self._collision_engine.calculate_collisions, actors)
+        measure_ns(self.engine_delegate.digest_events)
+        measure_ns(self._process_events)
+        measure_ns(self.engine_delegate.clear_display)
+        measure_ns(self._draw_actors)
+        measure_ns(self._notify_end_tick_to_actors)
+        measure_ns(self.engine_delegate.end_tick)
+        print('######################################################')
+
+def measure_ns(method, *args, **kwargs):
+    def wrapped_func():
+        tA = time.perf_counter_ns()
+        ret = method(*args, **kwargs)
+        tB = time.perf_counter_ns()
+        print(f'{method}: {(tB-tA)/1000000.0} ms')
+        return ret
+    return wrapped_func()
